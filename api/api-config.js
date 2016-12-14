54b6c061f27ef7049028d3fb4bd381f4f9729dac
Aapeli Haanpuu <aapzu@iki.fi>
2016-12-14 11:05:34 +0200

    Update and rename api-uri-config.js to api-config.js

---
+++ api/api-config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    uris: {
+        restaurants: "http://messi.hyyravintolat.fi/publicapi/restaurants",
+        menu: function(id) {
+            if (id === undefined)
+                throw "Id is required!"
+            return "http://messi.hyyravintolat.fi/publicapi/restaurant/" + id
+        }
+    },
+    areacodes: {
+        1: "Keskusta",
+        2: "Kumpula",
+        3: "Meilahti",
+        5: "Viikki",
+        6: "Metropolia"
+    }
+}
--- api/api-uri-config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    restaurants: "http://messi.hyyravintolat.fi/publicapi/restaurants",
-    menu: function(id) {
-        if (id === undefined)
-            throw "Id is required!"
-        return "http://messi.hyyravintolat.fi/publicapi/restaurant/" + id
-    }
-}
